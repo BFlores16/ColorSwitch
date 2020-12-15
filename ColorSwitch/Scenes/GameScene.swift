c523edfd6ef7238a8ac8c69abb3c8856ba3b0147
Brando <brandoflores16@gmail.com>
2020-12-15 16:34:24 -0700

    Add SKSpriteNode for color circle and ball

--- ColorSwitch/Scenes/GameScene.swift
+++ ColorSwitch/Scenes/GameScene.swift
@@ -9,8 +9,30 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var colorCircle: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
+        layoutScene()
+    }
+    
+    func layoutScene() {
+        backgroundColor = UIColor(red: 44/255, green: 66/255, blue: 80/255, alpha: 1.0)
         
+        colorCircle = SKSpriteNode(imageNamed: "ColorCircle")
+        colorCircle.size = CGSize(width: frame.size.width / 3, height: frame.size.width / 3)
+        colorCircle.position = CGPoint(x: frame.midX, y: frame.minY + colorCircle.size.height)
+        
+        addChild(colorCircle)
+        
+        spawnBall()
+    }
+    
+    func spawnBall() {
+        let ball = SKSpriteNode(imageNamed: "ball")
+        
+        ball.size = CGSize(width: 30.0, height: 30.0)
+        ball.position = CGPoint(x: frame.midX, y: frame.maxY)
+        addChild(ball)
     }
     
 }
